815c8b03d1c12bd811e84326e0f1ae8a07fef9ee
CauZy-Goes <cauafariasdev@gmail.com>
2024-05-24 12:16:32 -0300

    feat: create AssignedTaskService Interface. Close #5

---
+++ src/main/java/ucsal/codehub/lista_de_tarefas_designadas/model/services/AssignedTaskService.java
@@ -0,0 +1,15 @@
+package ucsal.codehub.lista_de_tarefas_designadas.model.services;
+
+import ucsal.codehub.lista_de_tarefas_designadas.model.entities.AssignedTask;
+
+import java.util.List;
+
+public interface AssignedTaskService {
+
+    void addAssignedTask(AssignedTask assignedTask);
+    void deleteAssignedTaskById(int id);
+    void updateAssignedTask(AssignedTask assignedTask);
+    AssignedTask getAssignedTaskById(int id);
+    List<AssignedTask> getAllAssignedTasks();
+    List<AssignedTask> getAssignedTasksByPersonId(int personId);
+}
