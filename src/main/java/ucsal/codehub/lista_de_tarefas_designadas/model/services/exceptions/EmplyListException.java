f8bb4197d98bed6a8727e8cdf041cb59d3e3b49c
CauZy-Goes <cauafariasdev@gmail.com>
2024-05-27 16:54:29 -0300

    feat: add EmplyListException exception. Close #16

---
+++ src/main/java/ucsal/codehub/lista_de_tarefas_designadas/model/services/exceptions/EmplyListException.java
@@ -0,0 +1,8 @@
+package ucsal.codehub.lista_de_tarefas_designadas.model.services.exceptions;
+
+public class EmplyListException extends RuntimeException{
+
+    public EmplyListException(String message){
+        super(message);
+    }
+}
